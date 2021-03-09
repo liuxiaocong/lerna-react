44f9b5060e4720e0c0322910c1ab330df4d68f02
Evan Liu <evan.liu@shopee.com>
2021-03-09 14:19:20 +0800

    Fix lib conflit

--- packages/button/lib/index.js
+++ packages/button/lib/index.js
@@ -12,7 +12,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 var Button = _styledComponents["default"].button.withConfig({
   displayName: "src__Button",
   componentId: "hjweup-0"
-})(["background:red;color:#fff;border-radius:4px;cursor:pointer;font-size:1rem;font-weight:400;padding:9px 36px;"]);
+})(["background:yellow;color:#fff;border-radius:4px;cursor:pointer;font-size:1rem;font-weight:400;padding:9px 36px;"]);
 
 var _default = Button;
 exports["default"] = _default;
